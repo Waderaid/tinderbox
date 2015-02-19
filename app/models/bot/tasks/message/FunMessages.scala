0d0d8599f15bd881cb9496e7d23da93e971b1308
Waderaid <Waderaid@hotmail.com>
2015-02-19 13:58:09 -0700

    Update FunMessages.scala

--- app/models/bot/tasks/message/FunMessages.scala
+++ app/models/bot/tasks/message/FunMessages.scala
@@ -19,10 +19,23 @@ object FunMessages {
         left = None
       ))
     ),
+     MessageTree(
+      value = "{name} Have any crazy stories from your last adventure?",
+      right = Some(MessageTree(
+        value = "Sounds like fun!  I am a really boring person too, HaHa! I just go sailing to Catalina on weekends and scuba dive.  Do you have anything planned for the weekend?",
+        right = None,
+        left = None
+      )),
+      left = Some(MessageTree(
+        value = "I just talked to my psychic Cleo and she told me I will meet someone interesting today, Is she talking about you? HaHa",
+        right = None,
+        left = None
+      ))
+    ),
     MessageTree(
       value = "I can't wait to introduce you to my mom!",
       right = Some(MessageTree(
-        value = "Since mom showed everyone my naked baby pictures, I've been looking for ways to get her back.",
+        value = "Since mom showed everyone my naked baby pictures, I've been looking for ways to get her back.  Would it be weird if you both were the same age?? HaHa",
         right = None,
         left = None
       )),
